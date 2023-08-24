17df5599eb4785da54e7c9a3b31cae9bd2c441ff
Wilkin Novo <w@novo.ws>
2023-08-24 14:39:27 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::env;
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Domain {
+    name: String,
+    favicon: String,
+}
+
+fn block(domain: Domain) {
+    println!("blocking: {:?}", domain);
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        3 => {
+            let action = &args[1];
+            if action == "block" {
+                let name = &args[2];
+                let domain = Domain {
+                    name: name.to_string(),
+                    favicon: "google.com/favicon".to_string(),
+                };
+                println!("ACTION: {}", action);
+                println!("DOMAIN: {}", name);
+
+                block(domain);
+            } else {
+                println!("Missing block argument");
+            }
+        }
+        _ => println!("Missing proper arguments. ...example: block cnn.com"),
+    }
+}
